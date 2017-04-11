10f6606f9d7faad1d3969ccc439ab35a69787da1
Don Matteo <donkey.shark@gmail.com>
2017-04-11 16:15:51 +0200

    Update junkbl.sh

--- junkbl.sh
+++ junkbl.sh
@@ -1,2 +1,2 @@
-cat /tmp/extracted-JunkEmails.asc | tr , '\n' | sed 's/[{}]//g;s/^[ \t]*//;/^\s*$/d;s/\r$//g;s/$/\t 550/' | tail -n+3 > /etc/postfix/junkbl_access
+cat -v /tmp/extracted-JunkEmails.asc | tr , '\n' | sed 's/[{}]//g;s/^[ \t]*//;/^\s*$/d;s/\^M//g' | grep . | grep -v BlockedSendersAndDomains | sed 's/$/\t 550/' > /etc/postfix/junkbl_access
 postmap /etc/postfix/junkbl_access
