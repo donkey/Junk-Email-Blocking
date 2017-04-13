dbacb0bda27f9133e30b83068179a21999ecbabf
Don Matteo <donkey.shark@gmail.com>
2017-04-13 11:57:40 +0200

    Update junkbl.sh

--- junkbl.sh
+++ junkbl.sh
@@ -1,2 +1,6 @@
+# build postfix junk emails
 cat -v /tmp/extracted-JunkEmails.asc | tr , '\n' | sed 's/[{}]//g;s/[\t ]//g;/^$/d;s/\^M$//g;s/BlockedSendersAndDomains://g' | grep . | sort | uniq -u | sed 's/$/\t 550 message was classified as spam/'  > /etc/postfix/junkbl_access
 postmap /etc/postfix/junkbl_access
+# build postfix trusted emails
+cat -v /tmp/extracted-TrustedEmails.asc | tr , '\n' | sed 's/[{}]//g;s/[\t ]//g;/^$/d;s/\^M$//g;s/TrustedSendersAndDomains://g' | grep . | sort | uniq -u | sed 's/$/\t ok/' > /etc/postfix/trusted_access
+postmap /etc/postfix/trusted_access
