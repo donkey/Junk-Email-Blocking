401b82d70ac273807d5d5a2af78c6613c6cd5f4d
Don Matteo <donkey.shark@gmail.com>
2017-04-13 09:28:56 +0200

    Update junkbl.sh

--- junkbl.sh
+++ junkbl.sh
@@ -1,2 +1,2 @@
-cat -v /tmp/extracted-JunkEmails.asc | tr , '\n' | sed 's/[{}]//g;s/[\t ]//g;/^$/d;s/\^M$//g;s/BlockedSendersAndDomains://g' | grep . | sort | uniq -u | sed 's/$/\t 550/'  > /etc/postfix/junkbl_access
+cat -v /tmp/extracted-JunkEmails.asc | tr , '\n' | sed 's/[{}]//g;s/[\t ]//g;/^$/d;s/\^M$//g;s/BlockedSendersAndDomains://g' | grep . | sort | uniq -u | sed 's/$/\t 550 message was classified as spam/'  > /etc/postfix/junkbl_access
 postmap /etc/postfix/junkbl_access
